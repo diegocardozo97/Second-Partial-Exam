6c82d800401efb59e5a06605eed3b80c3d599e74
diegocardozo97 <diego_a_cardozo@hotmail.com>
2020-05-08 09:56:27 -0500

    Second and third part

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -1,8 +1,36 @@
 const mongoose = require( 'mongoose' );
 const uuid = require( 'uuid' );
 
-/* Your code goes here */
+const schema = mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  num_players: {
+    type: Number,
+    required: true,
+  }
+});
+
+const sportsModel = mongoose.model("sports", schema);
 
 module.exports = {
-    
+  deleteSport: (id) => {
+    return sportsModel.remove({id}).then(status => {
+      return status;
+    }).catch(err => {
+      throw new Error("An db error happened:" + err);  
+    });
+  },
+  addSport: (body) => {
+    return sportsModel.insertMany([body]).then(status => {
+      return status;
+    }).catch(err => {
+      throw new Error("An db error happened:" + err);  
+    });
+  },
 };
--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -2,12 +2,45 @@ const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
+
 const { DATABASE_URL, PORT } = require( './config' );
+const SportsModel = require('./models/sport-model');
 
 const app = express();
 
 
-/* Your code goes here */
+app.delete("/sports/delete", jsonParser, (req, res) => {
+    if (!req.body.id || !req.query.sportId) {
+        res.statusMessage = "Not the id included";
+        return res.status(406).end();
+    }
+    if (req.body.id !== req.query.sportId) {
+        res.statusMessage = "Not the same  id";
+        return res.status(409).end();
+    }
+
+    SportsModel.deleteSport(req.body.id).then(status => {
+        console.log(status)
+        if (status.deletedCount === 0) {
+            res.statusMessage = "Id not found";
+            return res.status(404).end(); 
+        }
+        return res.status(204).end();
+    }).catch(err => {
+        res.statusMessage = "DB eerror:" + err;
+        return res.status(500).end();
+    });
+});
+
+app.post("/sports/add", jsonParser, (req, res) => {
+    SportsModel.addSport(req.body).then(status => {
+        console.log(status);
+        return res.status(200).end();
+    }).catch(err => {
+        res.statusMessage = "DB eerror:" + err;
+        return res.status(500).end();
+    });
+});
 
 
 app.listen( PORT, () => {
@@ -32,3 +65,9 @@ app.listen( PORT, () => {
         console.log( err );
     });
 });
+
+
+/*
+sudo systemctl start mongod
+
+*/
